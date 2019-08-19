3479958d67f8fabccec2ee2a0d65bf644d70fd7c
Mateusz Rusak <rusakkk@live.com>
2019-08-19 22:49:50 +0200

    create login form on login screen

--- src/components/login-screen/login-screen.js
+++ src/components/login-screen/login-screen.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import Styled from 'styled-components';
+import './login-screen.css';
 
-const MainScreen = Styled.div `
-    color: black;
-    
-`;
 
 class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            screen: 0,
+            login: '',
+            password: ''
+        }
+        this.toggleMode = this.toggleMode.bind(this);
+    }
+
+    toggleMode(event) {
+
+    }
+
     render() {
         return(
-            <div>
-
+            <div className="login-screen">
+                <header>
+                    @rusio chat app
+                </header>
+                <div className="buttons">
+                    <button onClick={this.toggleMode} className="btn-mode">Login</button>
+                    <button onClick={this.toggleMode} className="btn-mode">Register</button>
+                </div>
+                <div className="forms">
+                    <form action="" onSubmit={this.props.loggerService}>
+                        <input type="text" onChange={this.loginHandler} className="input-field" value={this.state.login} />
+                        <input type="password" onChange={this.pwHandler} className="input-field" value={this.state.password} />
+                        <input type="submit" value="Login!" className="btn-submit" />
+                    </form>
+                </div>
+                
             </div>
         );
     };
--- src/components/main-screen/main-screen.js
+++ src/components/main-screen/main-screen.js
@@ -5,13 +5,21 @@ import LoginScreen from './../login-screen/login-screen'
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: null,
+      loggedIn: false
+    }
+    this.loggerService = this.loggerService.bind(this);
+  }
 
+  loggerService(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="App">
-        <LoginScreen />
+        <LoginScreen loggerService={this.loggerService}/>
       </div>
     );
   }
