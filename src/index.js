c713736adfe839e17d875452d1deaf67750f5445
Mateusz Rusak <rusakkk@live.com>
2019-10-01 19:29:55 +0200

    create store object to keep messages locally

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {CookiesProvider} from 'react-cookie';
 import {BrowserRouter} from 'react-router-dom';
 import App from './app';
+import Store from './middlewares/store'
 
 
 ReactDOM.render(
     <CookiesProvider>
         <BrowserRouter>
-            <App messagesstore={[]}/>
+            <App store={Store}/>
         </BrowserRouter>
     </CookiesProvider>
     , document.getElementById('root'));
